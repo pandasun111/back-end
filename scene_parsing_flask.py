c847cc3fd6e80d0711b5865372d1d18fa4d1f644
lstcutong <527083553@qq.com>
2021-08-16 16:27:27 +0800

    update

--- scene_parsing_flask.py
+++ scene_parsing_flask.py
@@ -5,7 +5,7 @@ import threading
 import time
 from flask import request
 from flask import jsonify
-app = Flask(__name__)
+app = Flask(__name__, static_folder='./Data')
 CORS(app, supports_credentials=True)
 
 @app.route('/getallsceneinfo', methods=['POST'])
@@ -20,7 +20,21 @@ def getallsceneinfo():
 @app.route('/getsceneinfobyid', methods=['POST'])
 @cross_origin()
 def getsceneinfobyid():
-	pass
+	res = {
+		"scene_name": "Scene_0010",
+		"scene_scan_date": time.time(),
+		"scene_video_length": "00:30:00",
+		"scene_type": "indoor",
+		"scene_clutter_num": 30,
+		"scene_rgb_url": "http://114.212.81.162:4001/Data/Test/t1.mp4",
+		"scene_depth_url": "http://114.212.81.162:4001/Data/Test/t1.mp4",
+		"scene_lidar_url": "http://114.212.81.162:4001/Data/Test/t1.mp4",
+		"scene_rgb_recon_url": "http://114.212.81.162:4001/Data/Test/rgb.ply",
+		"scene_depth_recon_url": "http://114.212.81.162:4001/Data/Test/seman_rgb.ply",
+		"scene_semantic_rgb_recon_url": "http://114.212.81.162:4001/Data/Test/rgb.ply",
+		"scene_semantic_depth_recon_url": "http://114.212.81.162:4001/Data/Test/seman_rgb.ply"
+	}
+	return jsonify({"res": res})
 
 
 if __name__ == '__main__':
